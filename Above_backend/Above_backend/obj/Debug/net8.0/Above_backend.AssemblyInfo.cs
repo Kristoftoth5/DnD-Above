764119842393c2c6bc9d80be0a087c8728b59861
Andu762 <gaz20049@gmail.com>
2025-03-10 12:59:28 +0100

    Falmingo
    Falmingo

--- Above_backend/Above_backend/obj/Debug/net8.0/Above_backend.AssemblyInfo.cs
+++ Above_backend/Above_backend/obj/Debug/net8.0/Above_backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Above_backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d8c263233fd06895393f10e32fc77c5ec12708e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f87760b7b727e09f95b54814ab9d182aa1885ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("Above_backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Above_backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
