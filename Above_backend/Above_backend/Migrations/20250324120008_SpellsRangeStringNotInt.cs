0a9d13c250a61a05bf045e43dc886e8ec7415527
Andu762 <gaz20049@gmail.com>
2025-03-24 13:06:48 +0100

    Cantrips in db, fixed range type in spells
    Cantrips loaded into db
    Changed Range to string from int
    Falmingo

---
+++ Above_backend/Above_backend/Migrations/20250324120008_SpellsRangeStringNotInt.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Above_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class SpellsRangeStringNotInt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Range",
+                table: "Spells",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Range",
+                table: "Spells",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+    }
+}
--- Above_backend/Above_backend/obj/Debug/net8.0/Above_backend.AssemblyInfo.cs
+++ Above_backend/Above_backend/obj/Debug/net8.0/Above_backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Above_backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2e99f2438c5c6821c135e2efd8b9c3e7fcc8b80")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85acd2dfd83bceb4be56662af1838e1ccedbf21d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Above_backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Above_backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
