541ced0559e19758fced4a406b8075460ee13968
matetorok1223 <110980574+matetorok1223@users.noreply.github.com>
2025-04-09 11:04:19 +0200

    SpellCard mostly done
    Done most of the stuff, there might be kinks to iron out with the given Data in the future(spellcastingabilitymodifier, spellcaster and halfcaster specifically)
    
    Falmingo

--- Above_backend/Above_backend/obj/Debug/net8.0/Above_backend.AssemblyInfo.cs
+++ Above_backend/Above_backend/obj/Debug/net8.0/Above_backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Above_backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+251c1ca225f92c5c8ae95533f8723f76e31956be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56b6955c2eac6d1a553e2c3ec4cdd98e4186f567")]
 [assembly: System.Reflection.AssemblyProductAttribute("Above_backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Above_backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- dndabovefrontend/src/assets/CreatorPageCards/SpellCard.jsx
+++ dndabovefrontend/src/assets/CreatorPageCards/SpellCard.jsx
@@ -3,7 +3,6 @@ import spellLevelCalc from "../CommonFunctions/spellLevelCalc";
 import fetchEverything from "../CommonFunctions/fetchEverything";
 import profCalc from "../CommonFunctions/profCalc";
 import "../Cards.css"; 
-
 function SpellCard({ClassId})
 {
     const [spells, setSpells] = useState([]);
@@ -14,9 +13,15 @@ function SpellCard({ClassId})
     const [proficiencyBonus, setProficiencyBonus] = useState(2);
     const [dropdownOpen, setDropdownOpen] = useState(false);
     const [chosenSpellLevel, setChosenSpellLevel] = useState(undefined);
-    const [chosenSpells, setChosenSpells] = useState([]);
+    const [chosenSpellIds, setChosenSpellIds] = useState([]);
+    const [chosenSpellsLimit, setChosenSpellsLimit] = useState(1);
+    const [chosenCantripsLimit, setChosenCantripsLimit] = useState(2);
+    const [chosenCantripsNumber, setChosenCantripsNumber] = useState(0);
+    const [chosenSpells, setChosenSpells] = useState([{}]);
 
     const [characterLevel] = useState(6);
+    const [spellCaster, setSpellCaster] = useState(0);
+    const [halfCaster, setHalfCaster] = useState(0);
     const [spellCastingAbilityModifier] = useState(5);
 
     const [lastFetchedClassId, setLastFetchedClassId] = useState(null);
@@ -25,16 +30,21 @@ function SpellCard({ClassId})
     useEffect(()=>{
         async function fetchSpells()
         {
-            console.log("ChosenClassId: "+ClassId)
             if (!ClassId || ClassId === lastFetchedClassId) return;
             try 
             {
                 const response = await fetchEverything("Spells/originclassid/"+ClassId);
+                const classData = await fetchEverything("Classes/"+ClassId)
                 console.log("Fetched Spells:", response);
                 if (response && Array.isArray(response)) 
                 {
                     setSpells(response); // Only set if it's an array
                     setLastFetchedClassId(ClassId);
+                }
+                if(classData && Array.isArray(classData))
+                {
+                    setSpellCaster(classData.spellcaster);
+                    setHalfCaster(classData.halfcaster);
                 }
                 else 
                 {
@@ -52,8 +62,6 @@ function SpellCard({ClassId})
     useEffect(() => {
         if(spells !== undefined)
         {
-            var halfCaster = false;
-            var spellCaster = true;
             var temp = [];
             
             setHighestSpellLevel(spellLevelCalc(spellCaster,halfCaster,characterLevel));
@@ -63,6 +71,8 @@ function SpellCard({ClassId})
             }
             console.log("nemhiszemel: " + temp)
             setAvailableSpellLevels(temp);
+            setChosenCantripsLimit(Math.floor(characterLevel/4));
+            setChosenSpellsLimit(characterLevel+spellCastingAbilityModifier);
 
 
             if (spells.length > 0 && highestSpellLevel !== undefined) {
@@ -74,35 +84,68 @@ function SpellCard({ClassId})
         
     }, [spells, highestSpellLevel]);
 
-    function spellSelect(id)
+    useEffect(()=>{
+        var temparray = [{}]
+        async function fetchspells()
+        {
+            chosenSpellIds.forEach(async id =>{
+                temparray.push(await fetchEverything("Spells/"+id));
+            })
+        }
+        fetchspells();
+        if(temparray !== chosenSpells)setChosenSpells(temparray);
+    },[chosenSpellIds])
+
+
+    function spellSelect(id, level)
     {
-        var temparray = [...chosenSpells];
+        if(level == 0 & chosenCantripsNumber == chosenCantripsLimit)return;
+        else if(level != 0 & chosenSpellsLimit == chosenSpellIds.length)return; 
+        var temparray = [...chosenSpellIds];
+        var temp = 0;
+        level == 0 ? temp = chosenCantripsNumber : null;
         if(!temparray.includes(id)) temparray.push(id);
-        setChosenSpells(temparray);
-        console.log(chosenSpells);
+        temp++;
+        setChosenSpellIds(temparray);
+        level == 0 ? setChosenCantripsNumber(temp):null;
+        console.log(chosenSpellIds);
     }
 
-    function spellDeselect(id)
+    function spellDeselect(id, level)
     {
-        var temparray = [...chosenSpells];
-        temparray.length !== 0 ? temparray.splice(temparray.findIndex(element === id),1) : null;
-        setChosenSpells(temparray);
+        var temparray = [...chosenSpellIds];
+        var temp = 0;
+        level == 0 ? temp = chosenCantripsNumber : null;
+        var isId = (element) => element == id;
+        temparray.length !== 0 ? temparray.splice(temparray.findIndex(isId),1) : null;
+        temp--;
+        setChosenSpellIds(temparray);
+        level == 0 ? setChosenCantripsNumber(temp):null;
+        console.log(chosenSpellIds);
     }
 
     function spellRemoveDisplay(id)
     {
         var temparray = [...displayEligibleSpells];
         console.log(temparray)
-        temparray.length !== 0 ? temparray.splice((temparray.findIndex(element.id === id)),1):null;
+        var isId = (element) => element.id == id;
+        temparray.length !== 0 ? temparray.splice((temparray.findIndex(isId)),1):null;
         setDisplayEligibleSpells(temparray)
     }
 
-    function SpellAddDisplay(id)
+    async function SpellAddDisplay(id)
     {
-        
+        var temparray = [...displayEligibleSpells];
+        temparray.push(await fetchEverything("Spells/"+id))
+        setDisplayEligibleSpells(temparray)
     }
 
-    function SelectedSpells()
+    function showDetails(data)
+    {
+        window.alert(data);
+    }
+
+    function AvailableSpells()
     {
         return(
             <>
@@ -172,8 +215,8 @@ function SpellCard({ClassId})
                                 <td>{spell.range}</td>
                                 <td>{spell.components}</td>
                                 <td>{spell.duration}<sup>{spell.concentration == 1 ? (<b>C</b>):(null)}</sup></td>
-                                <td><button className="btn btn-primary">?</button></td>
-                                {chosenSpells.includes(spell.id)?(<td><button className="btn btn-primary" onClick={()=>{spellSelect(spell.id); spellRemoveDisplay(spell.id)}}>Add</button></td>):<td><button className="btn btn-primary" onClick={()=>{spellDeselect(spell.id); spellRemoveDisplay(spell.id)}}>Remove</button></td>}
+                                <td><button className="btn btn-primary" onClick={()=>{showDetails(spell.description)}}>?</button></td>
+                                {!chosenSpellIds.includes(spell.id)?(<td><button className="btn btn-primary" onClick={()=>{spellSelect(spell.id, spell.level); spellRemoveDisplay(spell.id)}}>Add</button></td>):<td><button className="btn btn-primary" onClick={()=>{spellDeselect(spell.id, spell.level); SpellAddDisplay(spell.id)}}>Remove</button></td>}
                             </tr>):null
                         ))}
                         </tbody>
@@ -186,16 +229,62 @@ function SpellCard({ClassId})
             </>
         )
     }
+
+    function SelectedSpells()
+    {   
+        return(
+            <>
+            {chosenSpells.length !== 0 ? (
+                <>
+                    <table>
+                            <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>School & Level</th>
+                                    <th>Casting Time</th>
+                                    <th>Range</th>
+                                    <th>Components</th>
+                                    <th>Duration</th>
+                                    <th>Details</th>
+                                    <th>Add Spell</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                        { chosenSpells.map((spell, index)=>(
+                            <tr key={index}>
+                                <td>{spell.name}</td>
+                                <td>Level {spell.level} {spell.school}</td>
+                                <td>{spell.castingTime}<sup>{spell.ritual == 1 ? (<i>R</i>):(null)}</sup></td>
+                                <td>{spell.range}</td>
+                                <td>{spell.components}</td>
+                                <td>{spell.duration}<sup>{spell.concentration == 1 ? (<b>C</b>):(null)}</sup></td>
+                                <td><button className="btn btn-primary" onClick={()=>{showDetails(spell.description)}}>?</button></td>
+                            </tr>
+                        ))}
+                        </tbody>
+                        </table>
+                        </>
+            ):null}
+            </>
+        )
+    }
     
 
 
     return(
+    <>
+        {spellCaster !== 0 || halfCaster !== 0 ?(
         <>
         <div className="creator-container">
             <h2 className="creator-title">Spells</h2>
-            <SelectedSpells/>
+            <AvailableSpells/>
+        </div>
+        <div className="selected-multiple">
+            {chosenSpellIds.length !== 0 ? (<SelectedSpells/>):null}
         </div>
         </>
+    ):null}
+    </>
     )
 }
 
