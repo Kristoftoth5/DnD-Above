9a965052cbe29a86e81efc3fdf724728a3df3755
Kristóf <115611714+Kristoftoth5@users.noreply.github.com>
2025-04-10 13:55:33 +0200

    Alert on Save
    falmingo(nagyF)

--- Above_backend/Above_backend/obj/Debug/net8.0/Above_backend.AssemblyInfo.cs
+++ Above_backend/Above_backend/obj/Debug/net8.0/Above_backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Above_backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3944ee08e0ee37a114359843f41d4400e9907185")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8ef6f1cc280a565d740f4c9078e1d72aa0e87f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Above_backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Above_backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
