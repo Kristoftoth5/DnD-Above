4b45e656ce36c0dc8750aceb45ab2efb27edb79c
matetorok1223 <110980574+matetorok1223@users.noreply.github.com>
2025-04-10 14:00:59 +0200

    Started on the savedsheet loading
    Falmingo

--- dndabovefrontend/src/CollectedDataTest.jsx
+++ dndabovefrontend/src/CollectedDataTest.jsx
@@ -5,15 +5,21 @@ import { StatsContext } from "./assets/SaveContexts/StatContext.jsx"
 import { BgNameContext, BgDescContext, BgSkillsContext, BgToolContext } from "./assets/SaveContexts/BackgroundContext.jsx"
 import { EquipmentContext, RemainingGoldContext } from "./assets/SaveContexts/EquipmentContext.jsx"
 import { FinalSpellsContext } from "./assets/SaveContexts/FinalSpellContext.jsx"
+import fetchEverything from "./assets/CommonFunctions/fetchEverything.js";
 
 
 function CollectedDataTest()
 {
     const { selectedRaceId } = useContext(RaceIdContext);
+    const { raceFeatures, setRaceFeatures } = useState();
+    const [ raceData, setRaceData ] = useState();
     const { selectedSubraceId } = useContext(SubraceIdContext);
+    const { subRaceFeatures, setSubRaceFeatures } = useState();
 
     const { ClassId } = useContext(ClassIdContext);
+
     const { SubclassId } = useContext(SubclassIdContext);
+    const { subClassFeatures, setSubClassFeatures } = useState();
     const { ChosenClassFeatureId } = useContext(ChosenClassFeatureIdContext);    
 
     const { Stats } = useContext(StatsContext)
@@ -28,6 +34,8 @@ function CollectedDataTest()
 
     const { FinalSpells } = useContext(FinalSpellsContext);
 
+    const [save, setSave] = useState();
+
     useEffect(() => {
         console.log("All data like... ever")
         console.log("Race ID: " + selectedRaceId + "\nSubrace ID: " + selectedSubraceId + "\n\n") // Race
@@ -36,6 +44,23 @@ function CollectedDataTest()
         console.log("Bg name: " + BgName + "\nBg Desc: " + BgDesc + "\nSkillls: " + BgSkills + "\nTool: " + BgTool + "\n\n") // Bg
         console.log("Equipment: " + Equipment + "\nGold: " + RemainingGold + "\n\n") // Equipment
         console.log("Spells: " + FinalSpells +"\n\n") // Spells
+        var tempracedata
+        var tempsubracedata
+        var tempracefeatures
+        async function fetchallthedataever()
+        {
+            tempracedata = fetchEverything("Races/"+selectedRaceId)
+            tempracefeatures = fetchEverything("Features/originraceid/"+selectedRaceId)
+        }
+        fetchallthedataever();
+        setRaceFeatures(tempracefeatures);
+        setRaceData(tempracedata);
+        tempsave+="<><>"
+        raceData.map((element,id)=>(
+            tempsave+=""+element.name+""
+        ))
+        var tempsave = "";
+        
     })
 
     return (
