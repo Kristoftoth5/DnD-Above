db48b18e3b511f031ff1873ee79476a0f5361f0f
matetorok1223 <110980574+matetorok1223@users.noreply.github.com>
2025-04-10 14:01:17 +0200

    asd

--- Above_backend/Above_backend/obj/Debug/net8.0/Above_backend.AssemblyInfo.cs
+++ Above_backend/Above_backend/obj/Debug/net8.0/Above_backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Above_backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8ef6f1cc280a565d740f4c9078e1d72aa0e87f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b45e656ce36c0dc8750aceb45ab2efb27edb79c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Above_backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Above_backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
